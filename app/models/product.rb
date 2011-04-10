a22e683b29d8ec9363fa8c4d10d1a17de7091af4
illiatdesdindes <illiatdesdindes@gmail.com>
2011-04-10 23:22:15 +0200

    Exo from chapter 7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, :presence => true
+  #:presence => {:presence => true, :message => 'have not been mentionned.'}
+  #cette version change le message d'erreur
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
+  #validates :title, :length => {:minimum => 10}
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.' }
