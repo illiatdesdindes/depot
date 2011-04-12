d69893acf814fd2d801c0975b81fc952325124a7
illiatdesdindes <illiatdesdindes@gmail.com>
2011-04-12 07:44:00 +0200

    store and his functional test

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -14,6 +14,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select ".list_description", 3
   end
 
   test "should get new" do
